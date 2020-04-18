302487ee22445d01bfb3b86d431a935c41d92e70
Cameron <49985259+cameronsocialhardware@users.noreply.github.com>
2020-04-18 16:39:35 +0100

    Fan bug fix

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -420,18 +420,18 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-//#define E0_AUTO_FAN_PIN -1
+#define E0_AUTO_FAN_PIN PC5
 //#define E1_AUTO_FAN_PIN -1
 //#define E2_AUTO_FAN_PIN -1
 //#define E3_AUTO_FAN_PIN -1
 //#define E4_AUTO_FAN_PIN -1
 //#define E5_AUTO_FAN_PIN -1
-#define CHAMBER_AUTO_FAN_PIN PC5
+//#define CHAMBER_AUTO_FAN_PIN -1
 
-#define EXTRUDER_AUTO_FAN_TEMPERATURE 10
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 0
 #define EXTRUDER_AUTO_FAN_SPEED 255   // 255 == full speed
-#define CHAMBER_AUTO_FAN_TEMPERATURE 10
-#define CHAMBER_AUTO_FAN_SPEED 255
+//#define CHAMBER_AUTO_FAN_TEMPERATURE 10
+//#define CHAMBER_AUTO_FAN_SPEED 255
 
 /**
  * Part-Cooling Fan Multiplexer
