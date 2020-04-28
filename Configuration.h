8bd99db30134ec724392ff843667a350287dcfbe
Cameron <49985259+cameronsocialhardware@users.noreply.github.com>
2020-04-28 15:15:54 +0100

    Undefined MANUAL_Z_HOME_POS

--- Configuration.h
+++ Configuration.h
@@ -1349,7 +1349,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 //#define MANUAL_X_HOME_POS 0
 //#define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 0.1
+//#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
